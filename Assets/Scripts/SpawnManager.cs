8b2e0765f6a7fb9b9ff677d28e687621ff3f2ff0
Przekazmierczak <prze.kazmierczak@gmail.com>
2025-08-19 21:35:04 +0200

    Add SquareIndicator and SpawnManager

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemy;
+    // private Coroutine spawningEnemy;
+
+    public void SpawnWave()
+    {
+        StartCoroutine(SpawningEnemy(10));
+    }
+
+    IEnumerator SpawningEnemy(int numberOfEnemy)
+    {
+        while (numberOfEnemy > 0)
+        {
+            Instantiate(enemy, new Vector3(8, 0, 0), Quaternion.identity);
+            numberOfEnemy--;
+            yield return new WaitForSeconds(1f);
+        }
+    }
+}
---
+++ Assets/Scripts/SquareIndicator.cs
@@ -0,0 +1,49 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SquareIndicator : MonoBehaviour
+{
+    Collider2D floorCollider;
+    public GameObject SquareIndicat;
+    private Vector2 lastMousePos;
+
+    void Start()
+    {
+        floorCollider = GetComponent<Collider2D>();
+        lastMousePos = Mouse.current.position.ReadValue();
+    }
+
+    void Update()
+    {
+        Vector2 mousePos = Mouse.current.position.ReadValue();
+
+        if (mousePos != lastMousePos)
+        {
+            lastMousePos = mousePos;
+
+            Vector3 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
+            worldPos.z = 0f; // Make sure Z is 0 for 2D
+            Collider2D[] hits = Physics2D.OverlapPointAll(worldPos);
+
+            bool ifFloor = false;
+            foreach (Collider2D hit in hits)
+            {
+                if (hit == floorCollider)
+                {
+                    ifFloor = true;
+                    break;
+                }
+            }
+
+            SetIndicator(ifFloor, worldPos);
+        }
+    }
+
+    void SetIndicator(bool ifFloor, Vector2 worldPos)
+    {
+        SquareIndicat.SetActive(ifFloor);
+        Debug.Log(worldPos);
+        SquareIndicat.transform.position = new Vector2(Mathf.Round(worldPos.x), Mathf.Round(worldPos.y));
+    }
+}
