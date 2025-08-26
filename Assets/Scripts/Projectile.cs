435e69f02e980c100bd3207b5f9de1f3ab0c8b0b
Przekazmierczak <prze.kazmierczak@gmail.com>
2025-08-26 21:43:19 +0200

    Add Skeleton animation, rotate projectile

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -32,5 +32,12 @@ public class Projectile : MonoBehaviour
             target.TakeDamage(damage);
             Destroy(gameObject);
         }
+
+        // Rotate if moving
+        if (direction != Vector3.zero)
+        {
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
     }
 }
